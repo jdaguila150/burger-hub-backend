aec50f7bdbe6856eb7bfaef6db63a54454bf1222
Josue Aguila <aguila.ortega.josue.daniel@gmail.com>
2026-04-19 01:40:15 -0600

    items added to menu

--- server.js
+++ server.js
@@ -47,8 +47,30 @@ client.initialize();
 
 // Ruta del menú (La que ya tenías)
 const menuData = [
-    { id: 1, categoria: "Hamburguesas", nombre: "La Bestia Clásica", precio: 140 },
-    // ... tus otros platillos
+  {
+    id: 1,
+    categoria: "Hamburguesas",
+    nombre: "La Bestia Clásica",
+    descripcion: "Doble carne de res, queso cheddar derretido.",
+    precio: 140, // Lo cambiamos a número para poder sumarlo en el carrito
+    imagen: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=500&q=80"
+  },
+  {
+    id: 2,
+    categoria: "Hamburguesas",
+    nombre: "Pollo Crispy",
+    descripcion: "Pechuga empanizada y mayonesa picante.",
+    precio: 120,
+    imagen: "https://images.unsplash.com/photo-1615719413546-198b25453f85?auto=format&fit=crop&w=500&q=80"
+  },
+  {
+    id: 3,
+    categoria: "Entradas",
+    nombre: "Papas Gajo a la Francesa",
+    descripcion: "Porción de 300g de papas condimentadas.",
+    precio: 65,
+    imagen: "https://images.unsplash.com/photo-1576107232684-1279f3908594?auto=format&fit=crop&w=500&q=80"
+  }
 ];
 
 app.get('/api/menu', (req, res) => {
